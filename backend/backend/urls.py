9dcedd2a346abd430b5dc8cd456cdc1ca4358e28
Justyna Pietrzak <Justyna.Pietrzak@cluepoints.com>
2023-01-21 00:27:49 +0100

    Add base swagger config

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -16,11 +16,35 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Images API",
+        default_version="v1",
+        description="Test description",
+    ),
+    public=True,
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api", include("images.urls")),
+    path("api/", include("images.urls")),
+    re_path(
+        r"^swagger(?P<format>\.json|\.yaml)$",
+        schema_view.without_ui(cache_timeout=0),
+        name="schema-json",
+    ),
+    re_path(
+        r"^swagger/$",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    re_path(
+        r"^redoc/$", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"
+    ),
 ]
 
 if settings.DEBUG:
