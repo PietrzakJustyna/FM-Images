12c66419e1f783970349322872fce4119b485153
Justyna Pietrzak <Justyna.Pietrzak@cluepoints.com>
2023-01-21 15:53:00 +0100

    Fix image serialization

--- backend/images/serializers.py
+++ backend/images/serializers.py
@@ -4,10 +4,13 @@ from rest_framework import serializers
 
 class ImageSerializer(serializers.ModelSerializer):
     url = serializers.SerializerMethodField("get_image_url")
+    image_file = serializers.ImageField(
+        max_length=None, allow_empty_file=False, write_only=True
+    )
 
     def get_image_url(self, obj):
         return obj.image_file.url
 
     class Meta:
         model = Image
-        fields = ("id", "title", "height", "width", "url")
+        fields = ("id", "title", "height", "width", "url", "image_file")
