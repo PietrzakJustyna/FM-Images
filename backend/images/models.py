2f72a4a1da9c19d4938e3043446a7087877f4d9a
Justyna Pietrzak <Justyna.Pietrzak@cluepoints.com>
2023-01-21 17:22:07 +0100

    Add image resizing

--- backend/images/models.py
+++ backend/images/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from PIL import Image as PILImage
 
 
 class Image(models.Model):
@@ -6,3 +7,9 @@ class Image(models.Model):
     width = models.PositiveIntegerField()
     height = models.PositiveIntegerField()
     image_file = models.ImageField()
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        image = PILImage.open(self.image_file.path)
+        image = image.resize((self.width, self.height))
+        image.save(self.image_file.path)
