72553f9b1437079ae5d7ffc779cb40966f25c106
Justyna Pietrzak <Justyna.Pietrzak@cluepoints.com>
2023-01-23 12:46:37 +0100

    Fix models

--- backend/images/models.py
+++ backend/images/models.py
@@ -1,11 +1,12 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 from PIL import Image as PILImage
 
 
 class Image(models.Model):
     title = models.CharField(max_length=255)
-    width = models.PositiveIntegerField()
-    height = models.PositiveIntegerField()
+    width = models.PositiveIntegerField(validators=[MaxValueValidator(1024)])  # pixels
+    height = models.PositiveIntegerField(validators=[MaxValueValidator(1024)])  # pixels
     image_file = models.ImageField()
 
     def save(self, *args, **kwargs):
@@ -13,3 +14,6 @@ class Image(models.Model):
         image = PILImage.open(self.image_file.path)
         image = image.resize((self.width, self.height))
         image.save(self.image_file.path)
+
+    def __str__(self):
+        return self.title
