b3082edbce6106d0936fe71739412f56b24d732f
Justyna Pietrzak <Justyna.Pietrzak@cluepoints.com>
2023-01-24 18:55:34 +0100

    Use test credentials for aws

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -149,6 +149,3 @@ DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 AWS_ACCESS_KEY_ID = os.environ["AWS_ACCESS_KEY_ID"]
 AWS_SECRET_ACCESS_KEY = os.environ["AWS_SECRET_ACCESS_KEY"]
 AWS_STORAGE_BUCKET_NAME = os.environ["AWS_STORAGE_BUCKET_NAME"]
-
-# Your app endpoint
-AWS_S3_ENDPOINT_URL = os.environ.get("AWS_S3_ENDPOINT_URL")
--- backend/images/tests/conftest.py
+++ backend/images/tests/conftest.py
@@ -6,6 +6,17 @@ from images.tests.factories import ImageFactory
 from moto import mock_s3
 
 
+@pytest.fixture(autouse=True)
+def configure_settings(settings):
+    settings.REST_FRAMEWORK = {
+        "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+        "PAGE_SIZE": 1,
+    }
+    settings.AWS_ACCESS_KEY_ID = "testaccesskey"
+    settings.AWS_SECRET_ACCESS_KEY = "testsecretaccesskey"
+    settings.AWS_STORAGE_BUCKET_NAME = "testbucketname"
+
+
 @pytest.fixture
 def image_file():
     file = io.BytesIO(
@@ -34,14 +45,6 @@ def text_file_image_extension():
 @pytest.fixture
 def image_factory():
     return ImageFactory
-
-
-@pytest.fixture(autouse=True)
-def configure_settings(settings):
-    settings.REST_FRAMEWORK = {
-        "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-        "PAGE_SIZE": 1,
-    }
 
 
 @pytest.fixture(scope="function")
